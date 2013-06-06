422105a850625d868880cce1ebb60bc893c633a7
Toni Korpela <tmkcodes@gmx.com>
2013-06-06 23:00:35 +0300

    load search html

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once("./php/template.php");
 $template = new template("./html/base.html");
 
 $template->replace("[NAVBAR]", "./html/navbar.html");
-$pages = array("./html/welcome.html", "./html/login.html", "./html/build-log.html", "./html/new-profiles.html", "./html/picture-ad.html", "./html/profile.html", "./html/forum.html", "./html/games.html");
+$pages = array("./html/welcome.html", "./html/login.html", "./html/build-log.html", "./html/new-profiles.html", "./html/picture-ad.html", "./html/profile.html", "./html/search.html", "./html/forum.html", "./html/games.html");
 $template->replace("[CONTENT]", $pages);
 
 $template->send();
