a76cb3e892516029e1849e6d828fe934e7e01be0
Toni Korpela <tmkcodes@gmx.com>
2013-06-06 21:40:53 +0300

    new data

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-`git add \*`
-`git commit -am $1`
+git add \*
+git commit -am '$1'
 upto = `git pull`
 if[upto == "Already up-to-date.\n"]
 	then
-		`git push`
-		`rm -rf /home/TMKCodes/.gvfs/ftp:host=ftp2.shellit.org,user=u32169/public_html`
-		`mv -rf /home/TMKCodes/projects/kattellaan /home/TMKCodes/.gvfs/ftp:host=ftp2.shellit.org,user=u32169/public_html`
+		git push
+		rm -rf /home/TMKCodes/.gvfs/ftp:host=ftp2.shellit.org,user=u32169/public_html
+		mv -rf /home/TMKCodes/projects/kattellaan /home/TMKCodes/.gvfs/ftp:host=ftp2.shellit.org,user=u32169/public_html
 	else
 		echo "Pull conflict. Needs checking."
 fi 
