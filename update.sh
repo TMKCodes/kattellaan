43f11d36973373744ee2566e7118a96049a250e6
Toni Korpela <tmkcodes@gmx.com>
2013-06-06 21:52:07 +0300

    $1

--- update.sh
+++ update.sh
@@ -6,8 +6,6 @@ upto = `git pull`
 if[upto == "Already up-to-date.\n"]
 	then
 		git push
-		rm -rf /home/TMKCodes/.gvfs/ftp:host=ftp2.shellit.org,user=u32169/public_html
-		mv -rf /home/TMKCodes/projects/kattellaan /home/TMKCodes/.gvfs/ftp:host=ftp2.shellit.org,user=u32169/public_html
 	else
 		echo "Pull conflict. Needs checking."
 fi 
