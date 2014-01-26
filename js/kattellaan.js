1c8da8c8f4fc329d620707bd7ddcf00dffd4fe40
Toni Korpela <toni@mussukka.org>
2014-01-26 16:35:38 +0200

    add console log when navigation left link is clicked.

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -1,4 +1,5 @@
 $('#navigation-left > li').click(function() {
+	console.log("navigation-left link was clicked.");
 	$('#navigation-left').children().removeClass("active");
 	$(this).addClass("active");
 });
