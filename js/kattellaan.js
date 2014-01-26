17fea85f1665a4f50a26f81a2bfbfe620878241e
Toni Korpela <toni@mussukka.org>
2014-01-26 18:14:40 +0200

    change event to evt

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -1,16 +1,16 @@
 $("#navigation-left > li").click(function() {
-	event.preventDefault();	
+	evt.preventDefault();	
 	$("#navigation-left").children().removeClass("active");
 	$(this).addClass("active");
 });
 
 $("#home_link").click(function() {
-	event.preventDefault();
+	evt.preventDefault();
 	$("body > .container").hide();
 	$("#home").show();
 });
 
 $("#first_link").click(function() {
-	event.preventDefault();
+	evt.preventDefault();
 	$("body > .container").hide();
 });
