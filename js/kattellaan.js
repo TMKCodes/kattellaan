a215f5a302e58fad383c11b75e897bde9cb4f1e6
Toni Korpela <toni@mussukka.org>
2014-01-26 17:57:23 +0200

    add container hiding and showing events

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -3,3 +3,12 @@ $("#navigation-left > li").click(function() {
 	$("#navigation-left").children().removeClass("active");
 	$(this).addClass("active");
 });
+
+$("#home_link").click(function() {
+	$("body > .container").hide();
+	$("#home").show();
+});
+
+$("#first_link").click(function() {
+	$("body > .container").hide();
+}
