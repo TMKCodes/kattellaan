e733d13b943eb34eb13154bea01cb5a27f994da3
Toni Korpela <toni@mussukka.org>
2014-01-26 18:15:29 +0200

    add evt parameter to events

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -1,16 +1,16 @@
-$("#navigation-left > li").click(function() {
+$("#navigation-left > li").click(function(evt) {
 	evt.preventDefault();	
 	$("#navigation-left").children().removeClass("active");
 	$(this).addClass("active");
 });
 
-$("#home_link").click(function() {
+$("#home_link").click(function(evt) {
 	evt.preventDefault();
 	$("body > .container").hide();
 	$("#home").show();
 });
 
-$("#first_link").click(function() {
+$("#first_link").click(function(evt) {
 	evt.preventDefault();
 	$("body > .container").hide();
 });
