e5ad1cbcbc5f4671225130b367c6ff690dffd0eb
Toni Korpela <toni@mussukka.org>
2014-01-28 20:12:32 +0200

    print...

--- php/api.php
+++ php/api.php
@@ -4,6 +4,7 @@ require_once("dbwrapper/db.php");
 require_once("account.php");
 
 $database = new db("sqlite3");
+printf("Opened this fucking php file.");
 if($database->open("kattellaan.db") == true) {
 	printf("Connection success.\r\n");
 
