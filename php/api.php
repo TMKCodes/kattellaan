c49d277474551d1994a7004701cd35171a227f1a
Toni Korpela <toni@mussukka.org>
2014-01-28 17:00:44 +0200

    database connection test.

--- php/api.php
+++ php/api.php
@@ -1,5 +1,12 @@
 <?php
 
+require_once("db.php");
+require_once("account.php");
 
+$database = new db("sqlite3");
+if($db->open("kattellaan.db") == true) {
+	printf("Connection success.\r\n");
+
+}
 
 ?>
