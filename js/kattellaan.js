0958f0517c08574eeb59b2bfad24f563f74db152
Toni Korpela <toni@mussukka.org>
2014-01-28 16:02:15 +0200

    added ajax call and checks if data is empty

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -35,4 +35,25 @@ $("#register-button").click(function(evt) {
 
 $("#register-form").submit(function(evt) {
 	evt.preventDefault();
+	if($("#input-username").val().length() <= 0) {
+		$("#input-username-empty").show();
+	} else if($("#input-address").val().length() <= 0) {
+		$("#input-address-empty").show();
+	} else if($("#input-password").val().length() <= 0) {
+		$("#input-password-empty").show();
+	} else if($("#input-password-confirm").val().length() <= 0) {
+		$("#input-password-empty").show();
+	} else if($("#input-password").val() != $("#input-password-confirm")) {
+		$("#input-password-mismatch").show();
+	} else {
+		$.ajax({
+			type: $(this).attr('method'),
+			url: $(this).attr('action'),
+			data: $(this).serialize()
+		}).done(function(data){
+			$("body > .container").hide();
+			$("#invite-page").show();
+		});
+	}
 });
+
