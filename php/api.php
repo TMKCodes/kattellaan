b7cc866de9ba313d44e59dec710cccc3300f7704
Toni Korpela <toni@mussukka.org>
2014-01-28 21:22:36 +0200

    add sqlite3 open readwrite

--- php/api.php
+++ php/api.php
@@ -9,7 +9,7 @@ require_once("dbwrapper/db.php");
 require_once("account.php");
 
 $database = new db("sqlite3");
-if($database->open("kattellaan.db") == true) {
+if($database->open("kattellaan.db", SQLITE3_OPEN_READWRITE) == true) {
 
 	/// register new account event.
 	if(!empty($_GET['register'])) {
