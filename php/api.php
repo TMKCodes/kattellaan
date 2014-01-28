8899f3b09ba37841f8b7d8f2ec89877baf258d06
Toni Korpela <toni@mussukka.org>
2014-01-28 20:30:03 +0200

    remove print

--- php/api.php
+++ php/api.php
@@ -6,7 +6,6 @@ error_reporting(E_ALL);
 require_once("dbwrapper/db.php");
 require_once("account.php");
 
-print_r($_GET);
 $database = new db("sqlite3");
 if($database->open("kattellaan.db") == true) {
 
