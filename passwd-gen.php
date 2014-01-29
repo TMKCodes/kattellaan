576359e8f2bc64e2686a4fc00f59ab1e02633822
Toni Korpela <toni@mussukka.org>
2014-01-29 02:58:00 +0200

    fix passwd gen not decode but encode

--- passwd-gen.php
+++ passwd-gen.php
@@ -1,6 +1,6 @@
 <?php
 if($argc == 3) {
-	file_put_contents(".passwd", base64_decode($argv[1] .":".$argv[2]));
+	file_put_contents(".passwd", base64_encode($argv[1] .":".$argv[2]));
 } else {
 	printf("This is simple passwd file script.\r\n");
 	printf("passwd-gen.php username password\r\n");
