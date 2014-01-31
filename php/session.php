6c26f9a6be6c26a5b34a10b86db348b890c9cdab
Toni Korpela <toni@mussukka.org>
2014-01-31 14:45:42 +0200

    replace remote host lookup with remote address lookup in session

--- php/session.php
+++ php/session.php
@@ -29,7 +29,7 @@ class session {
 
 	private function client() {
 		$client_user_agent = $_SERVER['HTTP_USER_AGENT'];
-		$client_remote_host = $_SERVER['REMOTE_HOST'];
+		$client_remote_host = $_SERVER['REMOTE_ADDR'];
 		$client_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
 		return hash($this->session_hash, $client_user_agent . "||" . $client_remote_host . "||" . $client_forwarded_for); 
 	}
