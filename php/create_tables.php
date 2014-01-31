3a83be5c7ca5f00ef33f34919d7cc35406061bfd
Toni Korpela <toni@mussukka.org>
2014-01-31 14:23:57 +0200

    fix .passwd location in create_tables

--- php/create_tables.php
+++ php/create_tables.php
@@ -3,7 +3,7 @@ require_once("dbwrapper/db.php");
 require_once("account.php");
 require_once("session.php");
 
-$passwd = explode(":", base64_decode(file_get_contents("home/temek/kattellaan/.passwd")));
+$passwd = explode(":", base64_decode(file_get_contents("/home/temek/kattellaan/.passwd")));
 $database = new db("mysqli");
 if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true) {
 	$account = new account($database);
