c6953bb5fa4046b2b6217d0d4476cfe8b253f6dd
Toni Korpela <toni@mussukka.org>
2014-01-31 18:29:22 +0200

    ...

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -112,6 +112,7 @@ $("#invite-form").submit(function(evt) {
 		for(var index in result) {
 			if(result[index] != true) {
 				count = count + 1;
+				console.log(result[index][0]);
 				$("#input-friend-addresses").append(cloneinput);
 				$("#input-friend-addresses").children(":last").children("label").attr("for-friend-address-" + count);
 				$("#input-friend-addresses").children(":last").children("label").html("<h2>Tälle ystävälle kutsun lähettäminen epäonnistui.</h2>");
