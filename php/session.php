c7932108e31542ab2380bd4e30ced0dc84163dc9
Toni Korpela <toni@mussukka.org>
2014-02-01 12:47:55 +0200

    fix sql syntax problem in session

--- php/session.php
+++ php/session.php
@@ -102,7 +102,7 @@ class session {
 	public function confirm($data) {
 		if(!empty($data)) {
 			$data = explode("||", base64_decode($data));
-			$statement = $this->database->prepare("SELECT * FROM `session` WHRE `id` = '?' AND `secret` = '?' AND `client` = '?';");
+			$statement = $this->database->prepare("SELECT * FROM `session` WHERE `id` = ? AND `secret` = ? AND `client` = ?;");
 			$statement->bind("s", $data[0]);
 			$statement->bind("s", $data[1]);
 			$statement->bind("s", $this->client());
