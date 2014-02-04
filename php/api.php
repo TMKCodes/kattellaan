5a813bcfb48e765c90af3b4bc3abf063ab5b481e
Toni Korpela <toni@mussukka.org>
2014-02-04 22:24:23 +0200

    fix progress bar id

--- php/api.php
+++ php/api.php
@@ -134,7 +134,7 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 			die();
 		}
 	} else if(!empty($_POST['call']) && $_POST['call'] == "upload") {
-		if(!empty($_POST['session'])) {
+		if(empty($_POST['session'])) {
 			/*$session = new session($database, "sha512");
 			if($session->confirm($_POST['session']) == false) {
 				printf('{ "success": false, "error": "Failed to confirm session" }');
