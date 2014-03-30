22dfef4a638ff7dcae54881af6b82c13953b02bd
Toni Korpela <toni@mussukka.org>
2014-03-30 22:19:02 +0300

    add error reporting to table creation

--- php/create_tables.php
+++ php/create_tables.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+
 require_once("dbwrapper/db.php");
 require_once("account.php");
 require_once("session.php");
