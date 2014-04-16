093610107921209bb73993acc2db5b233c88b068
Toni Korpela <toni@mussukka.org>
2014-04-16 15:49:00 +0300

    console log result 0 in get_town

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -156,6 +156,7 @@ function get_town(latlng) {
 		console.log(data); 
 		var result = data;
 		if(result.status == "OK") {
+			console.log(result[0]);
 			for(var i = 0; i < result[0].address_components.length; i++) {
 				if(result[0].address_components[i].types[0] == "locality") {
 					town = result[0].address_components[i].long_name;
