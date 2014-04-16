cd10fbf354baa7ef0c4ece33731f8765c51c38e4
Toni Korpela <toni@mussukka.org>
2014-04-16 15:26:20 +0300

    ..

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -142,9 +142,9 @@ function register_select_profile_picture(picture) {
 
 function get_town(latlng) {
 	var town;
-	latlng = latlng.replace(/(/g, '');
-	latlng = latlng.replace(/)/g, '');
-	latlng = latlng.replace(/ /g, '');
+	latlng = latlng.replace(/\(/g, '');
+	latlng = latlng.replace(/\)/g, '');
+	latlng = latlng.replace(/\ /g, '');
 	console.log(latlng);
 	$.ajax({
 		url: "http://maps.googleapis.com/maps/api/geocode/json",
