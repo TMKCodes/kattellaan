1804eda2721cfc321de87114143c35093ee638c7
Toni Korpela <toni@mussukka.org>
2014-04-21 23:44:09 +0300

    fix distance insert

--- php/distance.php
+++ php/distance.php
@@ -93,15 +93,15 @@ class distance {
 			throw new Exception("Identifier already set.");
 			return false;
 		}
-		if(!empty($this->distance)) {
+		if(empty($this->distance)) {
 			throw new Exception("Distance is not set.");
 			return false;
 		}
-		if(!empty($this->start)) {
+		if(empty($this->start)) {
 			throw new Exception("Start position is not set.");
 			return false;
 		}
-		if(!empty($this->end)) {
+		if(empty($this->end)) {
 			throw new Exception("End position is not set.");
 			return false;
 		}
