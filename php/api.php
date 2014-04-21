e30fcd35fecb516ae3ad40657cccc083a1a47ef2
Toni Korpela <toni@mussukka.org>
2014-04-22 02:55:55 +0300

    try fixing file upload bug.

--- php/api.php
+++ php/api.php
@@ -163,7 +163,7 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 			die();
 		}
 	} else if(!empty($_POST['call']) && $_POST['call'] == "upload") {
-		if(!empty($_POST['session'])) {
+		if(!empty($_COOKIE['session'])) {
 			$session = new session($database, "sha512");
 			if($session->confirm($_POST['session']) == false) {
 				printf('{ "success": false, "error": "Failed to confirm session" }');
