b289bbc67e434d17e52f81fed8b7b60c3a3deb39
Toni Korpela <toni@mussukka.org>
2014-04-22 05:36:51 +0300

    fix unexpected { in api.php on line 286

--- php/api.php
+++ php/api.php
@@ -283,7 +283,7 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 			printf('{ "success": false, "error": "Not authenticated." }');
 		}
 	} else if(!empty($_POST['call']) && $_POST['call'] == "get_distance") {
-		if(!empty($_COOKIE['SESSION']) {
+		if(!empty($_COOKIE['SESSION'])) {
 			$session = new session($database, "sha512");
 			if($session->confirm($_COOKIE['session']) == false) {
 				printf('{ "success": false, "error": "Failed to confirm session." }');
