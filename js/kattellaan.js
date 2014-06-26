086921400e56b60a3c3c6d3f484b1a9c5d6d6a10
Toni Korpela <toni@mussukka.org>
2014-06-26 17:34:11 +0300

    add some debugging to picture upload

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -690,7 +690,7 @@ $("document").ready(function() {
 			}
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
-			console.log("Status: " + textStatus + "\r\nError: " + errorThrown);
+			console.log("jqXHR: " + jqXHR + "\r\nStatus: " + textStatus + "\r\nError: " + errorThrown);
 		}, 
 		success: function(responseText, statusText, xhr, $form) {
 			console.log("File sent.");
