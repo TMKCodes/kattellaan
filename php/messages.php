6937efbc1df073ec9746f9482a85c3cef6686267
Toni Korpela <toni@mussukka.org>
2014-07-02 01:43:30 +0300

    some messages page work

--- php/messages.php
+++ php/messages.php
@@ -102,14 +102,15 @@ class message {
 			$statement->bind("i", $this->seen);
 			$statement->bind("s", $this->type);
 			$statement->bind("s", $this->message);
-			printf("Fuck you once!");
+			printf("Fuck you once!\r\n");
 		} else {
 			throw new Exception("No identifying data specified. Give identifier.");
 			return false;
 		}
 		$result = $statement->execute();
 		if($result->success() == true) {
-			printf("Fuck you twice!");
+			printf("Fuck you twice!\r\n");
+			printf("query = %s\r\n", $statement->get());
 			if($result->rows() > 0) {
 				$data = $result->fetch_object();
 				if(empty($data)) throw new Exception("Failed to fetch object.");
