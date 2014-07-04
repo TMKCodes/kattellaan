ea72821de82d180ba706d0902b71cd8f7404e914
Toni Korpela <toni@mussukka.org>
2014-07-04 16:41:30 +0300

    fix a bug

--- php/api.php
+++ php/api.php
@@ -383,7 +383,7 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 	} else if(!empty($_POST['call']) && $_POST['call'] == "get_unread_messages") {
 		if(!empty($_COOKIE['session'])) {
 			$session = new session($database, "sha512");
-			if($session->confirm($_COOKIE['session']) == false)
+			if($session->confirm($_COOKIE['session']) == false) {
 				printf('{ "success": false, "error": "Failed to confirm session." }');
 				die();
 			}
