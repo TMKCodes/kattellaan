75809eb880cdbaecb616a69b48a73c6594014cb8
Toni Korpela <toni@mussukka.org>
2014-07-07 19:03:03 +0300

    ..

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -732,9 +732,9 @@ function set_message_to_seen(mid) {
 					var count = parseInt($("#user-menu-messages > .dropdown-menu > #empty > a > #count").html());
 					count -= 1;
 					if(count > 0) {
-						$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\">" + count + "</i>" + count + " uutta viestiä.");
+						$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\" style=\"display: none;\">" + count + "</i>" + count + " uutta viestiä.");
 					} else {
-						$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\">0</i>Ei uusia viestejä.");
+						$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\" style=\"display: none;\">0</i>Ei uusia viestejä.");
 					}
 				}
 			});
@@ -750,7 +750,7 @@ function update_unread_messages() {
 	var unread = get_unread_messages("false");
 	// "{"success":true,"count":1,"messages":[{"mid":"125","sender_name":"TMKCodes","sender_uid":"214","receiver_name":"aa","receiver_uid":"189","timestamp":"2014-07-07 15:19:50","seen":"0","type":"text","message":"uusi viesti!"}]}"
 	if(unread.count != undefined) {
-		$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\">" + unread.count + "</i>" + unread.count + " uutta viestiä."); 
+		$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\" style=\"display: none;\">" + unread.count + "</i>" + unread.count + " uutta viestiä."); 
 		var list = "";
 		var shown = new Array();
 		$(".unread_message_id").each(function(i, obj) { shown.push($(obj).html()); } );
@@ -764,7 +764,7 @@ function update_unread_messages() {
 		}
 		$("#user-menu-messages > .dropdown-menu").prepend(list);
 	} else {
-		$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\">0</i>Ei uusia viestejä.");
+		$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\" style=\"display: none;\">0</i>Ei uusia viestejä.");
 	}
 }
 
