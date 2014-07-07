0ec6deef4811a466a12956eb3e83deaba0d6dea5
Toni Korpela <toni@mussukka.org>
2014-07-07 19:23:57 +0300

    ..

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -764,7 +764,7 @@ function update_unread_messages() {
 		}
 		$("#user-menu-messages > .dropdown-menu").prepend(list);
 		$(".notification > a").click(function() {
-			console.log($(this).children("#id").html());
+			console.log($(this + " > #id").html());
 		});
 	} else {
 		$("#user-menu-messages > .dropdown-menu > #empty > a").html("<i id=\"count\" style=\"display: none;\">0</i>Ei uusia viestejä.");
