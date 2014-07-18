96606b9bbf1af809adf492921f710e79d95bb6c1
Toni Korpela <toni@mussukka.org>
2014-07-19 00:33:56 +0300

    ..

--- php/api.php
+++ php/api.php
@@ -616,27 +616,27 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 			$query = "SELECT * FROM `account` INNER JOIN `profile` ON `id` = `identifier` WHERE ";
 			$ap_search_results = array();
 			if(!empty($_POST['username'])) {
-				$query += " `username` LIKE '%" . $_POST['username'] . "%'";
+				$query .= " `username` LIKE '%" . $_POST['username'] . "%'";
 			} else {
-				$query += " `username` LIKE '%%'";
+				$query .= " `username` LIKE '%%'";
 			}
 
 			if(!empty($_POST['age-min']) && !empty($_POST['age-max'])) {
 				$current_year = gmdate("Y");
 				$minimum_year = $current_year - $_POST['age-min'];
-				$query += " AND `birthday` < '" . $minimum_year . "-12-31'";
+				$query .= " AND `birthday` < '" . $minimum_year . "-12-31'";
 				$maximum_year = $current_year - $_POST['age-max'];
-				$query += " AND `birthday` > '" . $maximum_year . "-00-00'";
+				$query .= " AND `birthday` > '" . $maximum_year . "-00-00'";
 			}
 
 			if(!empty($_POST['gender'])) {
 				if(count($_POST['gender']) > 1) {
-					$query += " AND `gender` = '" . $_POST['gender'][0] . "'";
+					$query .= " AND `gender` = '" . $_POST['gender'][0] . "'";
 					for($i = 1; $i < count($_POST['gender']); $i++) {
 						$query += " OR `gender` = '" . $_POST['gender'][$i] . "'";
 					}
 				} else {
-					$query += " AND `gender` = '" . $_POST['gender'] ."'";		
+					$query .= " AND `gender` = '" . $_POST['gender'] ."'";		
 				}
 			}
 
