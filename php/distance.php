4fbc1c686591a4f9551bab7c7fdcaca77350ba14
Toni Korpela <toni@mussukka.org>
2014-07-25 14:52:39 +0300

    try to fix a bug in distance insert

--- php/distance.php
+++ php/distance.php
@@ -94,7 +94,7 @@ class distance {
 		}
 		$result = $statement->execute();
 		if($result->success() == true) {
-			if($result->rows() == 1) {
+			if($result->rows() >= 1) {
 				$data = $result->fetch_object();
 				if(empty($data)) throw new Exception("Failed to fetch object!");
 				$this->identifier = $data->id;
