ea23a41c2a3d5695902a4a6b1be85b571560f7fc
Toni Korpela <toni@mussukka.org>
2014-07-27 22:40:25 +0300

    change execution time calculation to microtime

--- php/do_distance_work.php
+++ php/do_distance_work.php
@@ -1,5 +1,5 @@
 <?php
-$stime = time();
+$stime = microtime();
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -39,6 +39,6 @@ if($database->connect("127.0.01", $passwd[0], $passwd[1], "kattellaan") == true)
 	printf("Failed to connect to database\r\n");
 }
 
-$etime = time();
+$etime = microtime();
 printf("Script executed in %s seconds.\r\n", $etime - $stime);
 ?>
