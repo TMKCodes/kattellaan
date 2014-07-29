a6a397e7791b0d41c0c3ad854da9e4d882ec7313
Toni Korpela <toni@mussukka.org>
2014-07-29 15:22:49 +0300

    change pagination button amount

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -1241,7 +1241,7 @@ $("#search-submit").click(function(evt) {
 				result_pagination += '<ul class="pagination">';
 					result_pagination += '<li id="search-result-prev-pagination-button" class="disabled"><a href="#">&laquo;</a></li>';
 					var paginations = data.result.length / 30;
-					for(var i = 0; i < ((paginations > 10) ? 11 : paginations); i++) {
+					for(var i = 0; i < ((paginations > 12) ? 12 : paginations); i++) {
 						var page = i + 1;
 						if(page == 1) {
 							result_pagination += '<li><a href="#" class="active">' + page + ' <span class="sr-only">(current)</span></a></li>';
@@ -1249,7 +1249,7 @@ $("#search-submit").click(function(evt) {
 							result_pagination += '<li><a href="#">' + page + '</a></li>';
 						}
 					}
-					if(paginations > 10) {
+					if(paginations > 12) {
 						result_pagination += '<li id="search-result-next-pagination-button"><a href="#">&raquo;</a></li>';
 					} else {
 						result_pagination += '<li id="search-result-next-pagination-button" class="disabled"><a href="#">&raquo;</a></li>';
