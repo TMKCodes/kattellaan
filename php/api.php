633751a4025bd3d527a5d7330f209aba2e7b33e7
Toni Korpela <toni@mussukka.org>
2014-07-29 14:47:56 +0300

    ..

--- php/api.php
+++ php/api.php
@@ -624,7 +624,7 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 			}
 
 
-			$query = "SELECT account.id, `username`, profile.address, `picture`, `relationship_status`, `registered`, `timestamp`, `birthday`, `gender`" .
+			$query = "SELECT account.id, `username`, profile.address, `profile_text` ,`picture`, `relationship_status`, `registered`, `timestamp`, `birthday`, `gender`" .
 					"FROM `account` " .
 					"INNER JOIN `profile` ON account.id = profile.identifier " .
 					"LEFT JOIN (SELECT * FROM `session` WHERE 1 ORDER BY `timestamp` DESC LIMIT 0, 1) s ON account.id = s.uid WHERE ";
