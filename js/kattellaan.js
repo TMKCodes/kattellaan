f2560c9ead38c35648b3ff950c80cfbc25ab1078
Toni Korpela <toni@mussukka.org>
2014-08-12 12:54:22 +0300

    add default profile picture and picture width

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -267,7 +267,11 @@ function load_latest_users_to_home_page() {
 			for(var i = 0; i < 6; i++) {
 				latest_users_display += '<div class="new-user col-xs-2 col-sm-2">';
 				latest_users_display += '<div class="uid" style="display: none;">' + result.users[i].id +  '</div>';
-				latest_users_display += '<div class="profile-picture"><img src="/uploads/' + result.users[i].picture + '" alt="' + result.users[i].username + '" /></div>';
+				if(result.users[i].picture != "") {
+					latest_users_display += '<div class="profile-picture"><img style="width: 100%;" src="/uploads/' + result.users[i].picture + '" alt="' + result.users[i].username + '" /></div>';
+				} else {
+					latest_users_display += '<div class="profile-picture"><img style="width: 100%;" src="/uploads/default.jpg" alt="' + result.users[i].username + '" /></div>';
+				}
 				latest_users_display += '</div>';
 				console.log(latest_users_display);
 			}
