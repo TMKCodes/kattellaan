59bab036fc5810da954faa8ae58baed032286e31
Toni Korpela <toni@mussukka.org>
2014-08-12 12:59:22 +0300

    add home page loading

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -1769,13 +1769,11 @@ $("document").ready(function() {
 				session = session.split("||");
 				load_edit_account_page(session[1]);
 			}
-		} else if(page === "") {
-			load_home_page();
 		} else {
 			load_page(page);
 		}
 	} else {
-		$("#home-page").show();
+		load_home_page();
 	}
 
 
