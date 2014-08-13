e091e07fd5c3664c4419a2df086f2f18100eacf6
Toni Korpela <toni@mussukka.org>
2014-08-13 17:09:29 +0300

    test selection

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -1336,8 +1336,8 @@ function load_edit_profile_page(uid) {
 	$("#height-edit-profile-input").val(profile.height);
 	$("#weight-edit-profile-input").val(profile.weight);
 	$("#income-edit-profile-input").val(profile.income);
-	$("#gender-edit-profile-input").children('option [value="man"]').selected();
-	$("#gender-edit-profile-input").parent().children("btn-group").multiselect("rebuild");
+	$("#gender-edit-profile-input").children('option [value="man"]').prop("selected", true);
+	$("#gender-edit-profile-input").parent().multiselect("refresh");
 	load_page("edit-profile-page");
 }
 
