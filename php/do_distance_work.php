70c0305de739f4600a49f37d71ead6b3e478d853
Toni Korpela <toni@mussukka.org>
2014-08-21 21:19:27 +0300

    ..

--- php/do_distance_work.php
+++ php/do_distance_work.php
@@ -20,30 +20,20 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 	$distance_work = $distance->get_uncalculated();
 	printf("%s\r\n", $distance_work);
 	if($distance_work != false) {	
-		$completed = false;
-		while($completed == false) {
-			$r = 6378137;
-			$dLat = rad($distance_work['end']['latitude'] - $distance_work['start']['latitude']);
-			$dLong = rad($distance_work['end']['longitude'] - $distance_work['end']['longitude']);
-			$a = sin($dLat / 2) * sin($dLat / 2) + cos(rad($distance_work['start']['latitude'])) * cos(rad($distance_work['end']['latitude'])) * sin($dLong / 2) * sin($dLong / 2);
-			$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-			$d = $r * $c;
+		$r = 6378137;
+		$dLat = rad($distance_work['end']['latitude'] - $distance_work['start']['latitude']);
+		$dLong = rad($distance_work['end']['longitude'] - $distance_work['end']['longitude']);
+		$a = sin($dLat / 2) * sin($dLat / 2) + cos(rad($distance_work['start']['latitude'])) * cos(rad($distance_work['end']['latitude'])) * sin($dLong / 2) * sin($dLong / 2);
+		$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+		$d = $r * $c;
 
-			$distance->set_start($distance_work['start']['identifier']);
-			$distance->set_end($distance_work['end']['identifier']);
-			$distance->set_identifier(null);
-			$distance->set_distance($d);
-			
-			try {
-				// printf("Distance:\r\nStart: %s\r\nEnd: %s\r\n", $distance->get_start(), $distance->get_end());
-				if($distance->insert() == true) {
-					$completed = true;
-				}
-			} catch(exception $e) {
-				continue;		
-			}
-		}
-		if($completed == true) {
+		$distance->set_start($distance_work['start']['identifier']);
+		$distance->set_end($distance_work['end']['identifier']);
+		$distance->set_identifier(null);
+		$distance->set_distance($d);
+		
+		// printf("Distance:\r\nStart: %s\r\nEnd: %s\r\n", $distance->get_start(), $distance->get_end());
+		if($distance->insert() == true) {
 			printf("Distance successfully calculated.\r\n");
 		}
 	}
