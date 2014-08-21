c7a427d0ee60e2aeb0cdff79a68c775a326756a1
Toni Korpela <toni@mussukka.org>
2014-08-21 21:20:19 +0300

    ..

--- php/do_distance_work.php
+++ php/do_distance_work.php
@@ -20,6 +20,7 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 	$distance_work = $distance->get_uncalculated();
 	printf("%s\r\n", $distance_work);
 	if($distance_work != false) {	
+		$completed = false;
 		$r = 6378137;
 		$dLat = rad($distance_work['end']['latitude'] - $distance_work['start']['latitude']);
 		$dLong = rad($distance_work['end']['longitude'] - $distance_work['end']['longitude']);
@@ -33,9 +34,14 @@ if($database->connect("127.0.0.1", $passwd[0], $passwd[1], "kattellaan") == true
 		$distance->set_distance($d);
 		
 		// printf("Distance:\r\nStart: %s\r\nEnd: %s\r\n", $distance->get_start(), $distance->get_end());
-		if($distance->insert() == true) {
-			printf("Distance successfully calculated.\r\n");
+		try {
+			if($distance->insert() == true) {
+				printf("Distance successfully calculated.\r\n");
+			}
+		} catch (Exception $e) {
+			
 		}
+
 	}
 } else {
 	printf("Failed to connect to database\r\n");
