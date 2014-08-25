982eec5b577bc563ada61940560a7e31d22b8979
Toni Korpela <toni@mussukka.org>
2014-08-25 23:55:17 +0300

    change email regex

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -2688,7 +2688,7 @@ $("#register-account-form").submit(function(evt) {
 	var username = $("#register-account-username-input").val();
 	if(username.length >= 0) {
 		var address = $("#register-account-address-input").val();
-		var addressRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		var addressRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 		if(address.length >= 0 && !addressRegex.test(address)) {
 			var password = $("#register-account-password-confirm-input").val();
 			if($("#register-account-password-input").val() == $("#register-account-password-confirm-input").val() && password.length >= 0 ) {
