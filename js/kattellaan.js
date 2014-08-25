4c63491494645dcf473d475943ca56742e1b9a56
Toni Korpela <toni@mussukka.org>
2014-08-25 13:05:06 +0300

    fix password recovery new password selection page url

--- js/kattellaan.js
+++ js/kattellaan.js
@@ -2323,7 +2323,7 @@ $("#navigation-left > li").click(function(evt) {
 });
 
 function load_new_password_page() {
-	var secret = get_url_parameter("secret");
+	var secret = "&secret=" + get_url_parameter("secret");
 	$("#new-password-form").prepend("<input type=\"hidden\" name=\"secret\" value=\"" + secret + "\" />");
 	load_custom_page("new-password-page", secret);	
 }
